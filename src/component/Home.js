a5776a24fe27db0128aea45de2f66202e69423d1
shubham <shubhamchaniyara309@gmail.com>
2024-03-12 18:27:34 +0530

    7th

--- src/component/Home.js
+++ src/component/Home.js
@@ -81,5 +81,55 @@ const Home= () => {
     
       };
 
-      
+      const handleStartQuiz = () => {
+        setQuizStarted(true);
+      };
+
+      const handleAnswerButtonClick = (selectedOption, answer) => {
+        const currentQuestion = questions[currentQuestionIndex];
+        if (!currentQuestion) {
+          return;
+        }
+        const correctAnswersMap = {
+          "answer_a": currentQuestion.correct_answers.answer_a_correct === "true" ? currentQuestion.answers.answer_a : null,
+          "answer_b": currentQuestion.correct_answers.answer_b_correct === "true" ? currentQuestion.answers.answer_b : null,
+          "answer_c": currentQuestion.correct_answers.answer_c_correct === "true" ? currentQuestion.answers.answer_c : null,
+          "answer_d": currentQuestion.correct_answers.answer_d_correct === "true" ? currentQuestion.answers.answer_d : null,
+          "answer_e": currentQuestion.correct_answers.answer_e_correct === "true" ? currentQuestion.answers.answer_e : null,
+          "answer_f": currentQuestion.correct_answers.answer_f_correct === "true" ? currentQuestion.answers.answer_f : null,
+        };
+        const correctAnswer = correctAnswersMap[selectedOption];
+        const isCorrect = answer === correctAnswer;
+        if (isCorrect) {
+          setScore(score + 1);
+        }
+        setSelectedOption(selectedOption);
+        setIsAnswerCorrect(isCorrect);
+      };
+      const handleNextButtonClick = () => {
+        if (currentQuestionIndex + 1 < questions.length) {
+          setCurrentQuestionIndex(currentQuestionIndex + 1);
+          setSelectedOption(null);
+          setIsAnswerCorrect(false);
+        } else {
+          setShowScore(true);
+        }
+      };
+      const restartQuiz = () => {
+        setScore(0);
+        setCurrentQuestionIndex(0);
+        setShowScore(false);
+        setQuestions([]);
+        setisRestart(!isRestart);
+        setTimeLeft(120); 
+      };
+      const handleEndQuiz = () => {
+        setShowScore(true);
+      };
+      const formatTime = (time) => {
+        const minutes = Math.floor(time / 60);
+        const seconds = time % 60;
+        return `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+      };
+    
 };
