bb86e6176914af642681481bd9bb30200ba0ab27
shubham <shubhamchaniyara309@gmail.com>
2024-03-12 17:36:14 +0530

    5th

---
+++ src/authorization/Welcome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './WelCome.css';
+
+const Welcome = () => {
+  return (
+    <div className="welcome-page">
+      <div className="navigation-buttons">
+        <Link to="/login"><button className="btn">Login</button></Link>
+        <Link to="/signup"><button className="btn">Signup</button></Link>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
