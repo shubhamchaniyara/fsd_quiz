ba16839f7003fa4751ba52f90f6447c7618f0490
shubham <shubhamchaniyara309@gmail.com>
2024-06-18 11:04:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SignUp from "./authorization/Signup";
 import Login from "./authorization/Login";
 import Welcome from "./authorization/Welcome";
@@ -8,20 +8,22 @@ import Home from "./component/Home";
 import JoinQuiz from "./component/JoinQuiz";
 import QuizPlay from "./component/QuizPlay";
 
+
 function App() {
   return (
     <div className="App">
-       <Router>
-            <Routes>
-                <Route path="/" element={<Welcome/>}/>
-                <Route path="/login" element={<Login/>}/>
-                <Route path="/signup" element={<SignUp/>}/>
-                <Route path="/home" element={<Home/>}/>
-                <Route path="/JoinQuiz" element={<JoinQuiz/>}/>
-                <Route path="/QuizPlay" element={<JoinQuiz/>}/>
-                <Route path="/Quizstart" element={<QuizPlay/>}/>
-            </Routes>
+      
+        <Router>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/JoinQuiz" element={<JoinQuiz />} />
+            <Route path="/Quizstart" element={<QuizPlay />} />
+          </Routes>
         </Router>
+     
     </div>
   );
 }
